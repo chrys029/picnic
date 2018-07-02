7b43392bb4eb1bdaf181b41717bbe27ffd998b8e
Chrystelle Bihan <cbn@edifixio.com>
2018-07-02 14:47:10 +0200

    withComponentAndRoutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './components/connexion/connexion.component';
+import { AccountComponent } from './components/account/account.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { AppRoutingModule} from './appRoutes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppRoutingModule,
+    AppComponent,
+    ConnexionComponent,
+    AccountComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    AuthGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class AppBootstrapModule {}
